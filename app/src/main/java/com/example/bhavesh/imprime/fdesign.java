7d3f0988d6b8e47a9faeca6e91807c2f91acb1c6
Bhavesh Badwal <Bhavesh Badwal>
2017-04-04 15:49:46 +0530

    logo retrieval from database

--- app/src/main/java/com/example/bhavesh/imprime/fdesign.java
+++ app/src/main/java/com/example/bhavesh/imprime/fdesign.java
@@ -16,6 +16,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -48,7 +50,7 @@ public class fdesign extends Fragment{
     private static final int PICK_IMAGE_REQUEST = 234;
     private ImageView user_image;
     private ImageView i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14;
-    private ImageView i15, i16, i17, i18, i19, i20, i21, i22, i23, i24, i25, i26, i27, i28;
+    private ImageView i15, i16, i17, i18, i19, i20, i21, i22, i23, i24, i25, i26, i27, i28, i29, i30, i31, i32;
     private Button b_choose, b_upload;
     private Uri userimagepath;
     private FirebaseAuth firebaseAuth;
@@ -135,10 +137,176 @@ public class fdesign extends Fragment{
 
       //  mstorageref = FirebaseStorage.getInstance().getReference(STORAGE_PATH_UPLOADS);
         mstorageref = FirebaseStorage.getInstance().getReference();
+        // Reference to an image file in Firebase Storage
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference(STORAGE_PATH_UPLOADS);
+
+// ImageView in your Activity
+         i1 = (ImageView) view.findViewById(R.id.l1);
+         i2 = (ImageView) view.findViewById(R.id.l2);
+         i3 = (ImageView) view.findViewById(R.id.l3);
+         i4 = (ImageView) view.findViewById(R.id.l4);
+         i5 = (ImageView) view.findViewById(R.id.l5);
+         i6 = (ImageView) view.findViewById(R.id.l6);
+         i7 = (ImageView) view.findViewById(R.id.l7);
+         i8 = (ImageView) view.findViewById(R.id.l8);
+         i9 = (ImageView) view.findViewById(R.id.l9);
+         i10 = (ImageView) view.findViewById(R.id.l10);
+         i11 = (ImageView) view.findViewById(R.id.l11);
+         i12 = (ImageView) view.findViewById(R.id.l12);
+         i13 = (ImageView) view.findViewById(R.id.l13);
+         i14 = (ImageView) view.findViewById(R.id.l14);
+         i15 = (ImageView) view.findViewById(R.id.l15);
+         i16 = (ImageView) view.findViewById(R.id.l16);
+         i17 = (ImageView) view.findViewById(R.id.l17);
+         i18 = (ImageView) view.findViewById(R.id.l18);
+         i19 = (ImageView) view.findViewById(R.id.l19);
+         i20 = (ImageView) view.findViewById(R.id.l20);
+         i21 = (ImageView) view.findViewById(R.id.l21);
+         i22 = (ImageView) view.findViewById(R.id.l22);
+         i23 = (ImageView) view.findViewById(R.id.l23);
+         i24 = (ImageView) view.findViewById(R.id.l24);
+         i25 = (ImageView) view.findViewById(R.id.l25);
+         i26 = (ImageView) view.findViewById(R.id.l26);
+         i27 = (ImageView) view.findViewById(R.id.l27);
+         i28 = (ImageView) view.findViewById(R.id.l28);
+         i29 = (ImageView) view.findViewById(R.id.l29);
+         i30 = (ImageView) view.findViewById(R.id.l30);
+         i31 = (ImageView) view.findViewById(R.id.l31);
+         i32 = (ImageView) view.findViewById(R.id.l32);
+
+// Load the image using Glide
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("hp.svg"))
+                .into(i1);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("cn.svg"))
+                .into(i2);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("apple.svg"))
+                .into(i3);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("danger.svg"))
+                .into(i4);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("ktm.png"))
+                .into(i5);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("google.svg"))
+                .into(i6);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("intel.svg"))
+                .into(i7);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("lamborghini.svg"))
+                .into(i8);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("udacity.svg"))
+                .into(i9);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("wb.svg"))
+                .into(i10);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("batman.gif"))
+                .into(i11);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("arsenal.svg"))
+                .into(i12);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("audi.svg"))
+                .into(i13);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("chanel.svg"))
+                .into(i14);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("csk.svg"))
+                .into(i15);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("dell.svg"))
+                .into(i16);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("fcb.svg"))
+                .into(i17);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("ferrari.png"))
+                .into(i18);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("batsuper.jpg"))
+                .into(i19);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("gulf.svg"))
+                .into(i20);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("ironman.jpeg"))
+                .into(i21);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("kkr.svg"))
+                .into(i22);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("github.png"))
+                .into(i23);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("mi.svg"))
+                .into(i24);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("oracle.svg"))
+                .into(i25);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("playboy.svg"))
+                .into(i26);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("porsche.svg"))
+                .into(i27);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("realmadrid.svg"))
+                .into(i28);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("rollsroyce.svg"))
+                .into(i29);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("samsung.svg"))
+                .into(i30);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("walkman.svg"))
+                .into(i31);
+        Glide.with(this.getContext())
+                .using(new FirebaseImageLoader())
+                .load(storageReference.child("bmw.jpg"))
+                .into(i32);
 
 
         File localFile = null;
-        try {
+        try{
             localFile = File.createTempFile("images", "jpg");
         } catch (IOException e) {
             e.printStackTrace();
